581ae45e8bc54caeb139a90d64dd9efd44ea1832
eckolo <eckolo.0722@gmail.com>
2017-11-04 18:16:34 +0900

    設計書のフォント変えた

--- Memo/Design.mm
+++ Memo/Design.mm
@@ -1,4 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1509782339759" ID="ID_1749361804" MODIFIED="1509782463520" TEXT="&#x30ed;&#x30fc;&#x30b0;&#x30e9;&#x30a4;&#x30af;&#x64ec;&#x304d;"/>
+<node CREATED="1509782339759" ID="ID_1749361804" MODIFIED="1509786965443" TEXT="&#x30ed;&#x30fc;&#x30b0;&#x30e9;&#x30a4;&#x30af;&#x64ec;&#x304d;">
+<font NAME="&#x3086;&#x305f;&#x307d;&#x3093;&#xff08;&#x30b3;&#x30fc;&#x30c7;&#x30a3;&#x30f3;&#x30b0;&#xff09;" SIZE="12"/>
+</node>
 </map>
