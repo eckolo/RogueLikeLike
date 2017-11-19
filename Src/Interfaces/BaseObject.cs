94083427af017155feb04c9b54835b68a0d06708
eckolo <eckolo.0722@gmail.com>
2017-11-20 02:22:49 +0900

    ベースオブジェクトだけ追加してみる

---
+++ Src/Interfaces/BaseObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+abstract class BaseObject
+{
+    public string name => _name;
+    [SerializeField]
+    string _name;
+    public string description => _description;
+    [SerializeField]
+    string _description;
+}
