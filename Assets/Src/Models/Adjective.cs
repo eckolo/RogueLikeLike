8d93226d2d579c7e339f46f1e11431db8b979ca2
eckolo <eckolo.0722@gmail.com>
2017-11-20 21:00:11 +0900

    「属性」だと紛らわしいので「形容詞」に変更

---
+++ Assets/Src/Models/Adjective.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Src.Models
+{
+    public class Adjective : ScriptableObject, Stationery.INamed
+    {
+        public new string name => _name;
+        [SerializeField]
+        string _name;
+
+        public string description => _description;
+        [SerializeField]
+        string _description;
+    }
+}
