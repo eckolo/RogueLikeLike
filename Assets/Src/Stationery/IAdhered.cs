4d537892b0ab2b61902da7c90acbf8d90dbb8656
eckolo <eckolo.0722@gmail.com>
2017-11-20 21:15:32 +0900

    形容されたオブジェクトのインターフェース追加

---
+++ Assets/Src/Stationery/IAdhered.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Src.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Src.Stationery
+{
+    interface IAdhered
+    {
+        List<Adjective> adjectives { get; set; }
+        Adjective mainAdjective { get; set; }
+    }
+}
