249461ddf81158851c57b30f7fd7913adde84ed2
eckolo <eckolo.0722@gmail.com>
2017-11-21 21:53:17 +0900

    スキル周りのガワ作成

---
+++ Assets/Src/Infrastructure/SkillRepository.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Src.Domains;
+using Assets.Src.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Src.Infrastructure
+{
+    /// <summary>
+    /// スキルリポジトリ
+    /// </summary>
+    class SkillRepository : MonoBehaviour, IRepository<Skill, int>
+    {
+        /// <summary>
+        /// 格納データ読み出し関数
+        /// </summary>
+        /// <param name="key">読み出しデータキー</param>
+        /// <returns>読みだされたデータ</returns>
+        public Skill GetResource(int key)
+        {
+            if(key < 0) throw new IndexOutOfRangeException();
+            if(key > _allSkillList.Count) throw new IndexOutOfRangeException();
+            return _allSkillList[key];
+        }
+
+        /// <summary>
+        /// 全スキルリスト
+        /// </summary>
+        [SerializeField]
+        List<Skill> _allSkillList = new List<Skill>();
+    }
+}
---
+++ Assets/Src/Models/Skill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Src.Models
+{
+    /// <summary>
+    /// スキルオブジェクト
+    /// </summary>
+    class Skill : ScriptableObject, Stationery.INamed
+    {
+        /// <summary>
+        /// スキル名称
+        /// </summary>
+        public new string name => _name;
+        /// <summary>
+        /// スキル名称設定箇所
+        /// </summary>
+        [SerializeField]
+        string _name;
+
+        /// <summary>
+        /// スキル説明文
+        /// </summary>
+        public string description => _description;
+        /// <summary>
+        /// スキル説明文章設定箇所
+        /// </summary>
+        [SerializeField]
+        string _description;
+    }
+}
