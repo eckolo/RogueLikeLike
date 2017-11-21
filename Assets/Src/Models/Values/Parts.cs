08051762afe408b0ecb7e6a6d1d55ee0c813b431
eckolo <eckolo.0722@gmail.com>
2017-11-22 08:10:19 +0900

    部位オブジェクト追加

---
+++ Assets/Src/Models/Values/Parts.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Assets.Src.Models;
+
+namespace Assets.Src.Models.Values
+{
+    /// <summary>
+    /// 部位オブジェクト
+    /// </summary>
+    public class Parts : Named
+    {
+    }
+}
