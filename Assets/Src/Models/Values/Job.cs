8514d65eab9f09b639a203201be4e3658160fe76
eckolo <eckolo.0722@gmail.com>
2017-11-22 08:04:25 +0900

    ジョブ関連のオブジェクト追加

---
+++ Assets/Src/Models/Values/Job.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Src.Models.Values
+{
+    /// <summary>
+    /// ジョブオブジェクト
+    /// </summary>
+    public class Job : Named
+    {
+    }
+}
