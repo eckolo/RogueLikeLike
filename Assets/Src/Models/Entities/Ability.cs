91982113411c2ac37718a96337d7f94a752db206
eckolo <eckolo.0722@gmail.com>
2017-11-24 01:06:32 +0900

    アビリティ関連オブジェクトのガワ追加

---
+++ Assets/Src/Models/Entities/Ability.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Src.Models.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Src.Models.Entities
+{
+    /// <summary>
+    /// アビリティオブジェクト
+    /// </summary>
+    public class Ability : AbilityStationery, IAdhered
+    {
+        /// <summary>
+        /// 形容詞リスト
+        /// </summary>
+        public List<Adjective> adjectives { get; set; }
+
+        /// <summary>
+        /// 主要形容詞
+        /// </summary>
+        public Adjective mainAdjective => adjectives.First();
+    }
+}
---
+++ Assets/Src/Models/Values/Motion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Src.Models.Values
+{
+    /// <summary>
+    /// アビリティの具体的動作を示すデータ群
+    /// </summary>
+    public class Motion : Named
+    {
+    }
+}
