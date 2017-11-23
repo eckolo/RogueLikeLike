2089500ac1757a58a6b12749217875f8bce236ae
eckolo <eckolo.0722@gmail.com>
2017-11-24 02:59:25 +0900

    キャラクタリポジトリ追加

---
+++ Assets/Src/Models/Person/PersonRepository.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Src.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Src.Models.Person
+{
+    /// <summary>
+    /// キャラクタリポジトリ
+    /// </summary>
+    public class PersonRepository : ResourceRepository<PersonStationery>
+    {
+        /// <summary>
+        /// データ格納ディレクトリ
+        /// </summary>
+        protected override string directory => "Person/";
+    }
+}
