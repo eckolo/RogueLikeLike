bd1b9ecce32e4393de271305da5b3db5383ffd27
eckolo <eckolo.0722@gmail.com>
2017-11-24 00:48:57 +0900

    アイテム周りのオブジェクトのガワ追加

---
+++ Assets/Src/Models/Entities/Item.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Src.Models.Values;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Src.Models.Entities
+{
+    /// <summary>
+    /// アイテムオブジェクト
+    /// </summary>
+    public class Item : ItemStationery, IAdhered
+    {
+        /// <summary>
+        /// 形容詞リスト
+        /// </summary>
+        public List<Adjective> adjectives { get; set; }
+
+        /// <summary>
+        /// 主要形容詞
+        /// </summary>
+        public Adjective mainAdjective => adjectives.First();
+    }
+}
