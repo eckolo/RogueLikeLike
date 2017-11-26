0919cea53a119ad98e0e227c96cc1f58a5a50547
eckolo <eckolo.0722@gmail.com>
2017-11-26 21:14:21 +0900

    地域データ実体クラスに形容詞付きインターフェースが実装されてなかったので修正

--- Assets/Src/Models/Area/AreaRoot.cs
+++ Assets/Src/Models/Area/AreaRoot.cs
@@ -10,7 +10,7 @@ namespace Assets.Src.Models
     /// <summary>
     /// 地域オブジェクトルート
     /// </summary>
-    public class AreaRoot : AreaStationery
+    public class AreaRoot : AreaStationery, IAdhered
     {
         /// <summary>
         /// 他地域との接続情報
@@ -20,5 +20,15 @@ namespace Assets.Src.Models
         /// 内包するマップのリスト
         /// </summary>
         public List<Map> includeMapList { get; set; }
+
+        /// <summary>
+        /// 形容詞リスト
+        /// </summary>
+        public List<Adjective> adjectives { get; set; }
+
+        /// <summary>
+        /// 主要形容詞
+        /// </summary>
+        public Adjective mainAdjective => adjectives.First();
     }
 }
