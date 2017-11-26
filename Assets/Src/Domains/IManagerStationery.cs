98530b8d68c72d331f6dd92db59cf2e24ce703f5
eckolo <eckolo.0722@gmail.com>
2017-11-26 20:11:26 +0900

    管理系クラスの雛形追加

---
+++ Assets/Src/Domains/IManagerStationery.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Src.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Src.Domains
+{
+    /// <summary>
+    /// 管理系サービスの雛形
+    /// </summary>
+    /// <typeparam name="Entity">管理対象オブジェクトの型</typeparam>
+    /// <typeparam name="Stationery">管理対象オブジェクトの雛形となる型</typeparam>
+    public interface IManagerStationery<Entity, Stationery> where Entity : Stationery, IAdhered where Stationery : Named
+    {
+        /// <summary>
+        /// オブジェクトの新規生成関数
+        /// </summary>
+        /// <param name="stationery">雛形となるオブジェクト</param>
+        /// <param name="addableAdjectives">
+        /// 付与され得る形容詞リスト
+        /// 同要素を複数含むことで付与確率上昇
+        /// </param>
+        /// <param name="level">形容詞の最大付与数</param>
+        /// <returns>生成されたオブジェクト</returns>
+        Entity Generate(Stationery stationery, List<Adjective> addableAdjectives, int level = 1);
+        /// <summary>
+        /// オブジェクトの形容詞を付け替え・追加する関数
+        /// </summary>
+        /// <param name="origin">元オブジェクト</param>
+        /// <param name="index">変更したい形容詞インデックス</param>
+        /// <param name="setedAdjective">追加したい形容詞</param>
+        /// <returns>変更されたオブジェクト</returns>
+        Entity ResetAdjective(Entity origin, int index, Adjective setedAdjective);
+    }
+}
