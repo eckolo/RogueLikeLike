791aaaa8b64c4a11322b2b4dfc46795c2a50af0b
eckolo <eckolo.0722@gmail.com>
2017-11-29 08:01:28 +0900

    SkillParameterをインスペクタから触れるよう修正

--- Assets/Src/Models/SkillParameter.cs
+++ Assets/Src/Models/SkillParameter.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Src.Models
 {
     /// <summary>
     /// スキルに対して各エンティティが所持するスキル値のオブジェクト
     /// </summary>
+    [Serializable]
     public class SkillParameter
     {
         /// <summary>
         /// 対応スキル
         /// </summary>
-        public Skill skill { get; }
+        [SerializeField]
+        Skill _skill = default(Skill);
+        /// <summary>
+        /// 対応スキル
+        /// </summary>
+        public Skill skill => _skill;
+
         /// <summary>
         /// スキル値
         /// </summary>
-        public int value { get; set; }
+        [SerializeField]
+        int _value = 0;
+        /// <summary>
+        /// スキル値
+        /// </summary>
+        public int value
+        {
+            get {
+                return _value;
+            }
+
+            set {
+                _value = value;
+            }
+        }
     }
 }
