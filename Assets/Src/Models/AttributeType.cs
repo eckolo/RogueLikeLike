74b93ffb518d826cc6fdd34d85a54a61c7b1db7b
eckolo <eckolo.0722@gmail.com>
2017-11-29 07:46:02 +0900

    属性に一通り説明追記

--- Assets/Src/Models/AttributeType.cs
+++ Assets/Src/Models/AttributeType.cs
@@ -8,19 +8,61 @@ namespace Assets.Src.Models
     [Serializable]
     public enum AttributeType
     {
+        /// <summary>
+        /// 斬撃
+        /// </summary>
         SLASH,
+        /// <summary>
+        /// 打撃
+        /// </summary>
         BLOW,
+        /// <summary>
+        /// 突撃
+        /// </summary>
         THRUST,
+        /// <summary>
+        /// 衝撃
+        /// </summary>
         IMPACT,
+        /// <summary>
+        /// 炎
+        /// </summary>
         FLAME,
+        /// <summary>
+        /// 氷
+        /// </summary>
         ICE,
+        /// <summary>
+        /// 風
+        /// </summary>
         WIND,
+        /// <summary>
+        /// 重
+        /// </summary>
         GRAVITY,
+        /// <summary>
+        /// 光
+        /// </summary>
         LIGHT,
+        /// <summary>
+        /// 闇
+        /// </summary>
         DARKNESS,
+        /// <summary>
+        /// 雷
+        /// </summary>
         THUNDER,
+        /// <summary>
+        /// 地
+        /// </summary>
         EARTH,
+        /// <summary>
+        /// 命
+        /// </summary>
         LIFE,
+        /// <summary>
+        /// 毒
+        /// </summary>
         POISON,
     }
 }
