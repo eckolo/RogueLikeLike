de6fc68c51dbf82d02f6c41b3bf30e74cbc36f00
eckolo <eckolo.0722@gmail.com>
2017-11-29 07:39:31 +0900

    キャラクタの露出パラメータクラスと変換機構のガワだけ追加

--- Assets/Src/Models/Person/PersonStationery.cs
+++ Assets/Src/Models/Person/PersonStationery.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +9,7 @@ namespace Assets.Src.Models.Person
     /// <summary>
     /// キャラクター雛形オブジェクト
     /// </summary>
-    public class PersonStationery : Named
+    public partial class PersonStationery : Named
     {
         /// <summary>
         /// 初期スキルリスト
