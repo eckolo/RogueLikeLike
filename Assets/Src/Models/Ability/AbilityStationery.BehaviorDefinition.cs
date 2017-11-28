d4080c7811f583421c3af7f49b56c6e9382484b9
eckolo <eckolo.0722@gmail.com>
2017-11-29 07:56:32 +0900

    アビリティの挙動定義パラメータ追加

--- Assets/Src/Models/Ability/AbilityStationery.BehaviorDefinition.cs
+++ Assets/Src/Models/Ability/AbilityStationery.BehaviorDefinition.cs
@@ -10,7 +10,7 @@ namespace Assets.Src.Models.Ability
         /// 挙動定義パラメータ
         /// </summary>
         [Serializable]
-        class BehaviorDefinition
+        partial class BehaviorDefinition
         {
             /// <summary>
             /// 挙動定義リスト
@@ -51,6 +51,26 @@ namespace Assets.Src.Models.Ability
             /// 最小射程距離
             /// </summary>
             public uint minRange => _minRange;
+
+            /// <summary>
+            /// ターゲットタイプ
+            /// </summary>
+            [SerializeField]
+            TargetableType _targetableType = default(TargetableType);
+            /// <summary>
+            /// ターゲットタイプ
+            /// </summary>
+            public TargetableType targetableType => _targetableType;
+
+            /// <summary>
+            /// 影響を受けるスキルと影響度合い一覧
+            /// </summary>
+            [SerializeField]
+            List<SkillParameter> _affectedSkills = new List<SkillParameter>();
+            /// <summary>
+            /// 影響を受けるスキルと影響度合いリスト
+            /// </summary>
+            public List<SkillParameter> affectedSkillList => _affectedSkills;
         }
 
     }
