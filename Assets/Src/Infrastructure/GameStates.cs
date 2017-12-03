9eeeb9d6eed80aca84566eb7aa72866f50a5be59
eckolo <eckolo.0722@gmail.com>
2017-12-03 20:21:32 +0900

    状態保持者というよりは保持されている状態そのもの的な名称に変更

--- Assets/Src/Infrastructure/GameStates.cs
+++ Assets/Src/Infrastructure/GameStates.cs
@@ -10,22 +10,22 @@ namespace Assets.Src.Infrastructure
     /// 状態保持クラス
     /// シングルトンとして中身を保持する
     /// </summary>
-    public class StateHolder
+    public class GameStates
     {
         /// <summary>
         /// 実際のインスタンス
         /// </summary>
-        static StateHolder myself = null;
+        static GameStates myself = null;
 
         /// <summary>
         /// インスタンス生成用のプライベートなコンストラクタ
         /// </summary>
-        StateHolder() { }
+        GameStates() { }
 
         /// <summary>
         /// 状態（つまりStateHolder自身）を取得する関数
         /// </summary>
         /// <returns>既にインスタンス生成されていればそれを、無ければ新規生成して返す</returns>
-        public static StateHolder GetState() => myself ?? new StateHolder();
+        public static GameStates GetState() => myself ?? new GameStates();
     }
 }
