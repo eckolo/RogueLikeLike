119a0b3183ad9744c733ade3ad59670e667a80f0
eckolo <eckolo.0722@gmail.com>
2017-12-15 21:23:32 +0900

    MyselfTestを分離

---
+++ Assets/Editor/TEST.MyselfTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+public static partial class TEST
+{
+    /// <summary>
+    /// ユニットテストモジュール自体のテストモジュール
+    /// </summary>
+    public static class MyselfTest
+    {
+        /// <summary>
+        /// ユニットテスト自体が正常に行われているか否かの確認
+        /// 必ず成功するはずのテストを行う
+        /// </summary>
+        [Test]
+        public static void TestBase()
+        {
+            //Arrange
+            var gameObject = new GameObject();
+
+            //Act
+            //Try to rename the GameObject
+            var newGameObjectName = "testBase";
+            gameObject.name = newGameObjectName;
+
+            //Assert
+            //The object has a new name
+            Assert.AreEqual(newGameObjectName, gameObject.name);
+        }
+    }
+}
--- Assets/Editor/TEST.cs
+++ Assets/Editor/TEST.cs
@@ -1,34 +1,6 @@
-﻿using UnityEngine;
-using NUnit.Framework;
-
-/// <summary>
+﻿/// <summary>
 /// ユニットテストモジュール
 /// </summary>
 public static partial class TEST
 {
-    /// <summary>
-    /// ユニットテストモジュール自体のテストモジュール
-    /// </summary>
-    public static class Myself
-    {
-        /// <summary>
-        /// ユニットテスト自体が正常に行われているか否かの確認
-        /// 必ず成功するはずのテストを行う
-        /// </summary>
-        [Test]
-        public static void TestBase()
-        {
-            //Arrange
-            var gameObject = new GameObject();
-
-            //Act
-            //Try to rename the GameObject
-            var newGameObjectName = "testBase";
-            gameObject.name = newGameObjectName;
-
-            //Assert
-            //The object has a new name
-            Assert.AreEqual(newGameObjectName, gameObject.name);
-        }
-    }
 }
