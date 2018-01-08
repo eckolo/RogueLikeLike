d388bf7ee059e77d253c20cc3a96539c93f55caa
eckolo <eckolo.0722@gmail.com>
2018-01-08 19:59:58 +0900

    リストから最大値を持つ要素を抜き出すメソッドの実装

---
+++ Assets/Editor/TEST.EnumerableManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Src.Domains;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+public static partial class TEST
+{
+    /// <summary>
+    /// リスト関連の汎用操作クラスのテスト
+    /// </summary>
+    public static class EnumerableManagerTest
+    {
+        [Test]
+        public static void MaxKeysTest()
+        {
+            var list = new List<Vector2> { new Vector2(2, 2), new Vector2(6, -8), new Vector2(-6, 8), new Vector2(0, -1) };
+
+            var result = list.MaxKeys(elem => elem.magnitude);
+            Assert.AreEqual(result.Count(), 2);
+
+            var resultFirst = result.Single(elem => elem.x > 0);
+            var resultSecond = result.Single(elem => elem.x < 0);
+            Assert.AreEqual(resultFirst.x, 6);
+            Assert.AreEqual(resultFirst.y, -8);
+            Assert.AreEqual(resultSecond.x, -6);
+            Assert.AreEqual(resultSecond.y, 8);
+        }
+    }
+}
---
+++ Assets/Src/Domains/EnumerableManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Src.Domains
+{
+    /// <summary>
+    /// リスト関連の汎用操作クラス
+    /// </summary>
+    public static class EnumerableManager
+    {
+        /// <summary>
+        /// リストから所定の最大値を持つ要素を抽出する
+        /// </summary>
+        /// <typeparam name="TSource">リストを構成する要素型</typeparam>
+        /// <typeparam name="TComparable">最大値判定時の型</typeparam>
+        /// <param name="source">元リスト</param>
+        /// <param name="selector">最大値判定値算出関数</param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> MaxKeys<TSource, TComparable>(this IEnumerable<TSource> source, Func<TSource, TComparable> selector) where TComparable : IComparable<TComparable>
+        {
+            var maxValue = source.Max(selector);
+            return source.Where(elem => selector(elem).CompareTo(maxValue) == 0);
+        }
+    }
+}
