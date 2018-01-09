78b19c840f1c5c2a23c1ac23386207bc0f432e6b
eckolo <eckolo.0722@gmail.com>
2018-01-10 08:05:41 +0900

    形容詞オブジェクトに接頭辞としての名称プロパティを追加

--- Assets/Src/Models/Adjective.cs
+++ Assets/Src/Models/Adjective.cs
@@ -1,9 +1,20 @@
-﻿namespace Assets.Src.Models
+﻿using UnityEngine;
+
+namespace Assets.Src.Models
 {
     /// <summary>
     /// 形容詞オブジェクト
     /// </summary>
     public class Adjective : Named
     {
+        /// <summary>
+        /// 接頭辞としての名称
+        /// </summary>
+        [SerializeField]
+        string _prefixName = null;
+        /// <summary>
+        /// 接頭辞としての名称
+        /// </summary>
+        public string prefixName => _prefixName;
     }
 }
