f15b2f3fbcf168548fc544001809f3f87d227c5a
eckolo <eckolo.0722@gmail.com>
2018-01-21 13:17:34 +0900

    ログを残す系の処理を一括化

---
+++ Assets/Src/Domains/MessageManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Src.Domains
+{
+    /// <summary>
+    /// ログとか画面文字表示管轄クラス
+    /// </summary>
+    public static partial class MessageManager
+    {
+        /// <summary>
+        /// ログを残す
+        /// </summary>
+        /// <param name="logger">ログの種類</param>
+        /// <param name="logedText">ログに残されるべきテキスト</param>
+        /// <returns></returns>
+        public static string LeaveLog(this LogHub logger, string logedText)
+        {
+            var displayedSentences = $"【{logger.ToString()}】{logedText}";
+            Debug.Log(displayedSentences);
+            return displayedSentences;
+        }
+    }
+}
