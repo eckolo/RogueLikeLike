30472392150041d657a882b313021a36e5d0b108
eckolo <eckolo.0722@gmail.com>
2018-03-29 08:08:52 +0900

    文字列の暗号化処理クラスの実装

---
+++ Assets/Editor/TEST.CryptManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Src.Domains;
+using Assets.Src.Infrastructure;
+using NUnit.Framework;
+using UnityEngine;
+public static partial class TEST
+{
+    /// <summary>
+    /// 暗号化クラスのテストモジュール
+    /// </summary>
+    public static class CryptManagerTest
+    {
+        [Test]
+        public static void EncodeCryptDecodeCryptTest()
+        {
+            var text = "text";
+
+            var result1 = text.EncodeCrypt();
+            var result2 = result1.DecodeCrypt();
+
+            Assert.AreNotEqual(text, result1);
+            Assert.AreEqual(text, result2);
+        }
+    }
+}
