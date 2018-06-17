0cf1f64d0a68763d89fc29746d708ac3b7cf6205
eckolo <eckolo.0722@gmail.com>
2018-06-17 15:54:52 +0900

    発生事象一覧生成メソッドをガワだけ実装

--- Assets/Src/Domains/Service/HappenedManager.cs
+++ Assets/Src/Domains/Service/HappenedManager.cs
@@ -3,6 +3,7 @@ using Assets.Src.Domains.Models.Value;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace Assets.Src.Domains.Service
 {
@@ -25,12 +26,12 @@ namespace Assets.Src.Domains.Service
         }
 
         /// <summary>
-        /// 起点となるNPC、アビリティからそのターンの行動リストを生成する
+        /// 起点となるNPC、アビリティからそのターンの発生事象一覧を生成する
         /// </summary>
         /// <param name="state">現在のゲーム状態</param>
         /// <param name="actor">起点となる行動者</param>
         /// <param name="selected">行動選択内容</param>
-        /// <returns>ターン内行動リスト</returns>
+        /// <returns>ターン内発生事象一覧</returns>
         public static List<Happened> GenerateHappenedList(this GameState state, Npc actor, Selected selected)
         {
             if(selected == null) return new List<Happened>();
@@ -39,7 +40,64 @@ namespace Assets.Src.Domains.Service
             var direction = selected.direction;
             var movement = selected.movement;
 
+            return ability.behaviorList
+                .SelectMany(behavior => state.GenerateHappenedList(actor, behavior, target, direction, movement))
+                .Concat(new List<Happened> { state.GenerateMoveHappened(actor, movement, ability.moveAnimation) })
+                .ToList();
+        }
+
+        /// <summary>
+        /// 指定された動作内容クラスを起点とする発生事象一覧を生成する
+        /// </summary>
+        /// <param name="state">現在のゲーム状態</param>
+        /// <param name="actor">起点となる行動者</param>
+        /// <param name="behavior">起点となる動作内容</param>
+        /// <param name="targetPoint">動作目標地点</param>
+        /// <param name="direction">動作方向</param>
+        /// <param name="movement">行動に付随する移動量</param>
+        /// <returns>発生事象一覧</returns>
+        static List<Happened> GenerateHappenedList(
+            this GameState state,
+            Npc actor,
+            Behavior behavior,
+            Vector2 targetPoint,
+            Direction direction,
+            Vector2 movement)
+        {
+            var range = behavior.coverage.Move(targetPoint, direction);
+            var npcs = state.npcLayout
+                .Where(layout => range.OnTarget(layout.Key))
+                .Select(layout => layout.Value);
+
+            return npcs.SelectMany(npc => state.GenerateHappenedList(actor, behavior, npc)).ToList();
+        }
+        /// <summary>
+        /// 指定された行動内容の所定の対象への作用を起点とした発生事象一覧を生成
+        /// </summary>
+        /// <param name="state">現在のゲーム状態</param>
+        /// <param name="actor">起点となる行動者</param>
+        /// <param name="behavior">起点となる動作内容</param>
+        /// <param name="target">動作対象</param>
+        /// <returns></returns>
+        static List<Happened> GenerateHappenedList(this GameState state, Npc actor, Behavior behavior, Npc target)
+        {
             throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 移動のみの発生事象クラスを生成
+        /// </summary>
+        /// <param name="state">現在のゲーム状態</param>
+        /// <param name="actor">移動者</param>
+        /// <param name="movement">予定移動量</param>
+        /// <param name="effect">移動エフェクト</param>
+        /// <returns>移動のみの発生事象クラス</returns>
+        static Happened GenerateMoveHappened(this GameState state, Npc actor, Vector2 movement, EffectAnimation effect)
+        {
+            return Happened.builder
+                .Target(actor)
+                .Movement(actor.CorrectMoving(state.map, movement))
+                .Animation(effect)
+                .Build();
         }
     }
 }
