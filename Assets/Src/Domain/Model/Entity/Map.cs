266bd061b66754f12201f039fcfbef047943b9d5
eckolo <eckolo.0722@gmail.com>
2018-12-07 23:27:41 +0900

    マップチップの更新処理実装

--- Assets/Src/Domain/Model/Entity/Map.cs
+++ Assets/Src/Domain/Model/Entity/Map.cs
@@ -1,7 +1,9 @@
 using Assets.Src.Domain.Model.Abstract;
 using Assets.Src.Domain.Model.Value;
+using Assets.Src.Domain.Service;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Src.Domain.Model.Entity
@@ -70,6 +72,30 @@ namespace Assets.Src.Domain.Model.Entity
         /// z座標が大きいほど手前に表示
         /// </summary>
         public Dictionary<Vector2, MapChip> mapchipList => _mapchipList;
+
+        /// <summary>
+        /// マップチップの更新処理
+        /// </summary>
+        /// <param name="mapChip">設置したいマップチップ</param>
+        /// <param name="pivot">設置対象範囲の左下基準座標</param>
+        /// <param name="range">設置対象範囲の広さ</param>
+        /// <returns></returns>
+        public Map UpdateMapChip(
+            MapChip mapChip,
+            Vector2 pivot,
+            Vector2? range = null)
+        {
+            var targetRangeX = Enumerable.Range(pivot.GetIntX(), Mathf.CeilToInt(range?.x ?? 1));
+            var targetRangeY = Enumerable.Range(pivot.GetIntY(), Mathf.CeilToInt(range?.y ?? 1));
+
+            var updateMapChips = targetRangeX
+                .SelectMany(x => targetRangeY.Select(y => new Vector2(x, y)))
+                .ToDictionary(target => target, _ => mapChip);
+
+            _mapchipList = _mapchipList.UpdateOrInsert(updateMapChips);
+
+            return this;
+        }
 
         /// <summary>
         /// マップが初期状態であるか否か
